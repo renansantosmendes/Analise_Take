b9622570d73d212cf4776874c681c1fc7b338119
renansantos <renansantosmendes@gmail.com>
2018-04-19 18:10:27 -0300

    adding moead results in R2

--- moea_pareto_test.py
+++ moea_pareto_test.py
@@ -21,8 +21,8 @@ for line in file.values.tolist():
 	f1.append(float(line[0]))
 	f2.append(float(line[1]))
 
-print(f1)
-print(f2)
+# print(f1)
+# print(f2)
 
 
 
@@ -53,5 +53,21 @@ print(f2)
 plt.scatter(f1, f2,color='red',marker='^')
 
 
+
+
+path = '/home/renansantos/Área de Trabalho/GA Testes/'
+file = path + 'MOEAD_R2.txt'
+
+def get_data_from_txt(file,index):
+	file = open(file)
+	f = []
+	for line in file:
+		f.append(float(line.replace('[','').replace(']','').replace('\n','').replace(',','').split()[index]))
+	return f
+	
+f1 = get_data_from_txt(file, 0)
+f2 = get_data_from_txt(file, 1)
+
+plt.scatter(f1, f2,color='green',marker='o')
 plt.show()
 
