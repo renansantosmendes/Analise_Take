d0fba0d4a1febea87c95f2bfce5107d6facf769c
renansantos <renansantosmendes@gmail.com>
2018-04-20 09:00:19 -0300

    pareto charts

--- moea_pareto_test.py
+++ moea_pareto_test.py
@@ -21,20 +21,14 @@ for line in file.values.tolist():
 	f1.append(float(line[0]))
 	f2.append(float(line[1]))
 
-# print(f1)
-# print(f2)
 
+plt.style.use('fast')
+plt.scatter(f1, f2,color='blue',marker='x',label='NSGA-II (framework)')
 
-
-# plt.scatter(g1, g2,color='green',marker='^')
-plt.scatter(f1, f2,color='blue',marker='x')
-# plt.plot(x, y,'r-')
-# plt.xlim(0, 1)
-# plt.ylim(0, 1)
 plt.xlabel('Objective Function 1')
 plt.ylabel('Objective Function 2')
 
-# # plt.contour(X, Y, Z)
+
 file_name = 'nsgaii_renan_2.csv'
 
 file = open(path + file_name,'r')
@@ -46,17 +40,10 @@ for line in file.values.tolist():
 	f1.append(float(line[0]))
 	f2.append(float(line[1]))
 
-print(f1)
-print(f2)
-
-
-plt.scatter(f1, f2,color='red',marker='^')
-
-
-
+plt.scatter(f1, f2,color='red',marker='^',label='NSGA-II (implemented)')
 
 path = '/home/renansantos/Área de Trabalho/GA Testes/'
-file = path + 'MOEAD_R2.txt'
+file = path + 'MOEAD_R2_2.txt'
 
 def get_data_from_txt(file,index):
 	file = open(file)
@@ -68,6 +55,9 @@ def get_data_from_txt(file,index):
 f1 = get_data_from_txt(file, 0)
 f2 = get_data_from_txt(file, 1)
 
-plt.scatter(f1, f2,color='green',marker='o')
-plt.show()
+plt.scatter(f1, f2,color='green',marker='o',label='MOEAD')
+plt.legend(loc='upper right')
 
+# plt.xlim(20000, 70000)
+plt.grid(True)
+plt.show()
